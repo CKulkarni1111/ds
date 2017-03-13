ea96f9f51aea48b2039e219a37cca735b79b2161
Chaitanya Kulkarni <chaitanya.kulkarni@hgst.com>
2017-03-13 16:21:20 -0700

    sll : add test for append list.

--- sll/main.c
+++ sll/main.c
@@ -5,12 +5,21 @@
 int main(int argc, char *argv[])
 {
 	int i;
-	struct node *sl = NULL;
+	struct node *sla = NULL;
+	struct node *slb = NULL;
 
 	for (i = 0; i < 10; i++)
-		sll_append(&sl, i * 10);
-	sll_print(sl);
-	sll_delete(&sl);
-	sll_print(sl);
+		sll_append(&sla, i * 10);
+
+	for (i = 0; i < 10; i++)
+		sll_append(&slb, i * 100);
+
+	sll_print(sla);
+	sll_print(slb);
+	sll_append_list(&sla, &slb);
+
+	sll_print(sla);
+	sll_print(slb);
+	sll_delete(&sla);
 	return 0;
 }
